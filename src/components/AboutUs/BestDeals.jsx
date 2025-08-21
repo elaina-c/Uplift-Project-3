cfab8abe2eb6b74eed4091f59f7362c4e9cbdb7e
Elaine  Lim <limelaine14@gmail.com>
2025-08-21 18:07:24 +0800

    Update BestDeals.jsx

--- src/components/AboutUs/BestDeals.jsx
+++ src/components/AboutUs/BestDeals.jsx
@@ -9,7 +9,7 @@ const BestDeals = () => {
   return (
     <div className={styles.bestDeals}>
       <div className={styles.imageContainer}>
-        <img src="/Uplift-Project-3/BestDeals.jpg" alt="Best Deals" className={styles.image} />
+       <img src={BestDealsImg} alt="Best Deals" className={styles.image} />
       </div>
       <div className={styles.content}>
         <h3>Best Deals</h3>
@@ -27,4 +27,5 @@ const BestDeals = () => {
 };
 
 export default BestDeals;
+
 
