170dbf4c37e0dcb96bc21ece607e2b367ee05b79
Elaine Lim <limelaine14.gmail.com>
2025-08-21 19:27:47 +0800

    Fix merge conflicts in AboutUs components

--- src/components/AboutUs/Intro.jsx
+++ src/components/AboutUs/Intro.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import styles from "./Intro.module.css";
 import Header from "../Header/Header";
@@ -28,34 +27,3 @@ const Intro = () => {
 };
 
 export default Intro;
-=======
-import React from "react";
-import styles from "./Intro.module.css";
-import Header from "../Header/Header";
-
-const Intro = () => {
-  return (
-    <div className={styles.intro}>
-      <div className={styles.imageContainer}>
-        <img
-          src={`${import.meta.env.BASE_URL}Intro.jpg`}
-          alt="Intro"
-          className={styles.image}
-        />
-      </div>
-      <div className={styles.content}>
-        <h2>Welcome to TrendyHub!</h2>
-        <p>
-          Discover the latest in fashion, lifestyle, and tech. At TrendyHub, we
-          bring you top-quality products that match your style and elevate your
-          everyday life. Our mission is simple: to make shopping exciting, easy,
-          and rewarding.
-        </p>
-        <Header />
-      </div>
-    </div>
-  );
-};
-
-export default Intro;
->>>>>>> 8473c062bbf124fe676bda7456a797f447f3b901
